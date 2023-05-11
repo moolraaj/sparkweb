741cc0f494118c58ff5a5a380759fe2f9f279954
moolraaj <116911577+moolraaj@users.noreply.github.com>
2023-05-11 14:04:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
+import './components/style.css'
+import React, { useState } from 'react'
 import './App.css';
+import Navbar from './components/Navbar';
+import Herosection from './components/Herosection';
+import AboutUs from './components/AboutUs';
+import WhatWeHaveDone from './components/WhatWeHaveDone';
+import Testimonials from './components/Testimonials';
+import Footer from './components/Footer';
+import OurServices from './components/OurServices';
+import sun from './components/images/sun.png'
+import moon from './components/images/moon.png'
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { Model } from './components/Newdance';
+ 
 
+ 
+ 
+ 
+ 
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+   {/* --light & dark theme function made here to use everywhere--*/ }
+ 
+   const [theme, setTheme] = useState('light')
+   const [img, setImg] = useState(sun)
+   
+
+   const toggleMode = () => {
+      if (theme === 'light') {
+         setTheme('dark');
+         document.body.style.background = "#0C073E";
+          
+         setImg(moon)
+         
+         
+          
+      }
+      else {
+         setTheme('light');
+         document.body.style.background = "#fff";
+         
+         setImg(sun)
+        
+        
+      }
+
+   }
+   return (
+      <>
+       
+         {/* --navbar-section--*/}
+         <Navbar  theme={theme} toggleMode={toggleMode} />
+
+
+         {/* --hero-section--*/}
+         {/* <Canvas className="cnavas" camera={{ position: [1, 1.5, 2.5], fov: 50 }}>
+         <ambientLight />
+      <directionalLight position={[-5, 5, 5]} castShadow shadow-mapSize-width={1024} shadow-mapSize-height={1024} />
+            <OrbitControls/>
+            <group position={[0, -1, 0]}>
+            <Model/>
+            </group>
+            <mesh rotation={[-0.5 * Math.PI, 0, 0]} position={[0, -1, 0]} receiveShadow>
+        <planeBufferGeometry args={[10, 10, 1, 1]} />
+        <shadowMaterial transparent opacity={0.2} />
+      </mesh>
+         </Canvas> */}
+         
+         <Herosection   img={img} theme={theme} toggleMode={toggleMode} />
+      
+
+
+         {/* --about-us-aection--*/}
+         <AboutUs   theme={theme} toggleMode={toggleMode} />
+
+         {/* --our-services-section--*/}
+         <OurServices theme={theme} toggleMode={toggleMode} />
+
+
+         {/* --what-we-have-done-section--*/}
+         <WhatWeHaveDone theme={theme} toggleMode={toggleMode} />
+
+
+         {/* --testimonials-section--*/}
+         <Testimonials theme={theme} toggleMode={toggleMode} />
+
+
+         {/* --footer-section--*/}
+         <Footer theme={theme} toggleMode={toggleMode} /> 
+          
+
+
+      </>
+   );
 }
 
 export default App;
---
+++ src/components/AboutUs.js
@@ -0,0 +1,41 @@
+import React from 'react'
+ 
+
+export default function AboutUs(props) {
+    return (
+        <>
+      
+   
+  
+
+            <div className='about-outer'>
+                <div className='about-inner'>
+                    <div className='about-flex'>
+                        <div className='about-left-section' style={{ color: props.theme === 'dark' ? "#fff" : "#000" }}>
+                            <h1 style={{ color: props.theme === 'dark' ? "#1E7CC0" : "#F24B74" }}>about us</h1>
+                            <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly
+                             used to demonstrate the visual form of a document or a typeface without relying
+                              on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
+                            
+                            <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly 
+                             used to demonstrate the visual form of a document or a typeface without relying 
+                             on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>                     </div>
+                        <div className='about-right-section'>
+                            <div className="about-first-img">
+                                <img src='my-images/frame-second.png' alt="img"/>
+                            </div>
+                            <div className="about-second-img">
+                                <img src='my-images/frame-first.png' alt="img"/>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div> 
+            
+        </>
+    )
+}
+
+
+
